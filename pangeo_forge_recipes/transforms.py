ddd63bbeeefd3cddbb81618a3817e53cc7817cb6
Raphael Hagen <norlandrhagen@gmail.com>
2024-03-14 11:32:55 -0600

    pdb

--- pangeo_forge_recipes/transforms.py
+++ pangeo_forge_recipes/transforms.py
@@ -724,7 +724,7 @@ class CreatePyramid(beam.PTransform):
 def regrid_level(item: Tuple[Index, xr.Dataset],level: int):
     index, ds = item
     dims = {'lat': level, 'lon': level}
-    # regrid!
+    import pdb; pdb.set_trace()
     regrid_ds = ds.coarsen(**dims).mean()
     return index, regrid_ds
 
